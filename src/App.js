e3c20bb01acfcdc9d3a6e17117d2af00a7b3d199
Heather <heather.hb@hotmail.com>
2026-03-16 14:38:30 -0600

    feat: scaffold React app with Dockerfile and Jenkins pipeline

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>React CI/CD with Docker & Jenkins</h1>
+        <p>Pipeline is running!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
